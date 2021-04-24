d67c48b5a70483f2d90f3c08175ea49fbe1ec4ef
Chris De Lima <delimac@hey.com>
2021-04-24 12:51:39 +0800

    update code for task crud

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,51 @@
 class TasksController < ApplicationController
   def index
     # this is the list page of all tasks
-    @tasks = ["Buy milk", "Do laundry", "Pay bills online"]
+    @tasks = Task.all
   end
 
   def new
+    # form for adding a new task
     @task = Task.new
+  end
+
+  def create
+    # take the info from the form and add it to the DB
+    @task = Task.new(task_params)
+    # save to the DB
+    @task.save
+    # redirect back to home
+    redirect_to root_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    # find the individual task
+    @task = Task.find(params[:id])
+    # destroy
+    @task.destroy
+    # redirect back to home
+    redirect_to root_path
+  end
+
+  def edit
+    # find the individual task
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    # find the individual task
+    @task = Task.find(params[:id])
+    # update with new info on form
+    @task.update(task_params)
+    # redirect to task view
+    redirect_to task_path(@task)
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
